f8c3debf803f8871dc8fe1c32154fe4757d6235a
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-01-14 17:13:34 +0100

    remove process.env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
   await app.listen(3000);
-  console.log(process.env);
 }
 bootstrap();
